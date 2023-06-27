7a17ca1187b730086090f0dc5630f49052b35cfa
Andrew <andy.nlarry@gmail.com>
2023-06-27 15:53:21 +0300

    Update

--- main.h
+++ main.h
@@ -3,17 +3,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * struct var_type - structure for the variable types
- * @type: type of variable
- * @f: function to be called
- */
-typedef struct var_type
-{
-	char type;
-	void (*f)();
-}var_type_t;
-
 int _printf(const char *format, ...);
 
 #endif/*MAIN_H*/
