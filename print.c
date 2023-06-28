2c0152b819709d632cdb51f97bd97baf0c3aade5
damaris-lab <damarisnyawiragacheru@gmail.com>
2023-06-27 19:58:10 -0700

    print.c

--- print.c
+++ print.c
@@ -4,17 +4,24 @@
 /**
  *_prints - prints a character
  *@arg:no of arguement
- *@va_list:pointer to arguement
+ *@va_list:pointer variable
  * Return:count
  */
-int _prints(arg, va_list)
+int _prints(va_list, arg)
 {
+	conversio m = {
+	{"%i", printf_int}, {"%d", printf_dec},
+	{"%c", printf_char}, {"%s", printf_string},
+	{"%%", printf_37},
+	{"%p", printf_pointer},
+};
+
 	char *str;
 	int count, count = 0;
 	int i, i = 0;
 
 
-	str = var_arg(list, char *str)
+	str = var_arg(char *str, arg)
 	while (str[i])
 	{
 		putchar (str[i];
