f6a3b9616671845b4a48a700ac0eaae4111b84fb
damaris-lab <damarisnyawiragacheru@gmail.com>
2023-06-27 19:21:06 -0700

    print.c

---
+++ print.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ *_prints - prints a character
+ *@arg:no of arguement
+ *@va_list:pointer to arguement
+ * Return:count
+ */
+int _prints(arg, va_list)
+{
+	char *str;
+	int count, count = 0;
+	int i, i = 0;
+
+
+	str = var_arg(list, char *str)
+	while (str[i])
+	{
+		putchar (str[i];
+		count++;
+		i++;
+	}
+	return (count);
